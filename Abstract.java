433eade40433379e259aadbecd4a1461e61dd4cb
wx <2214380963@qq.com>
2022-10-21 15:54:32 +0800

    java面向对象

--- Abstract.java
+++ Abstract.java
@@ -4,14 +4,15 @@ public class Abstract {
     }
 }
 
-// 抽象类
+// 抽象类（抽象类不能被实例化，只能用于继承）
 abstract class Person {
-    // 抽象方法
+    // 抽象方法（抽象方法必须在抽象类中）
     public abstract void run();
 }
 
 class Student extends Person {
     // 子类必须实现抽象父类的抽象方法
+    // @Override
     public void run() {
     }
 }
